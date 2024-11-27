8fcfe0a40b2f42794f9cda9480c9c7b4103157c3
Dennis Pohl <troftu@users.noreply.github.com>
2024-11-27 22:37:55 +0100

    Generate redirect pages

---
+++ src/pages/rowing/Definitions.ts
@@ -0,0 +1,9 @@
+class Rowing {
+	static LogbookProfileLink : string = "https://log.concept2.com/profile/1054524";
+	static LogbookLogLink : string = "https://log.concept2.com/profile/1054524/log";
+	static Logbook2kLink : string = "https://log.concept2.com/rankings/2025/rower/2000?age=&weight=&gender=&rower=rower&status=verified&page=9#411";
+	static Logbook5kLink : string = "https://log.concept2.com/rankings/2025/rower/5000?age=&weight=&gender=&rower=rower&status=verified&page=4#195";
+	static Logbook10kLink : string = "https://log.concept2.com/rankings/2025/rower/10000?age=&weight=&gender=&rower=rower&status=verified&page=5#208";
+}
+
+export default Rowing;

fd1ff4346e72adeae8e09bac9ca63a8ac80ff255
Dennis Pohl <troftu@users.noreply.github.com>
2024-11-28 20:45:35 +0100

    Update data

--- src/pages/rowing/Definitions.ts
+++ src/pages/rowing/Definitions.ts
@@ -3,7 +3,7 @@ class Rowing {
 	static LogbookLogLink : string = "https://log.concept2.com/profile/1054524/log";
 	static Logbook2kLink : string = "https://log.concept2.com/rankings/2025/rower/2000?age=&weight=&gender=&rower=rower&status=verified&page=9#411";
 	static Logbook5kLink : string = "https://log.concept2.com/rankings/2025/rower/5000?age=&weight=&gender=&rower=rower&status=verified&page=4#195";
-	static Logbook10kLink : string = "https://log.concept2.com/rankings/2025/rower/10000?age=&weight=&gender=&rower=rower&status=verified&page=5#208";
+	static Logbook10kLink : string = "https://log.concept2.com/rankings/2025/rower/10000?age=&weight=&gender=&rower=rower&status=verified&page=5#205";
 }
 
 export default Rowing;

720353a07258055040a57ec02ee239f92637f1ac
Dennis Pohl <troftu@users.noreply.github.com>
2024-12-19 18:29:46 +0100

    Update data

--- src/pages/rowing/Definitions.ts
+++ src/pages/rowing/Definitions.ts
@@ -1,9 +1,9 @@
 class Rowing {
 	static LogbookProfileLink : string = "https://log.concept2.com/profile/1054524";
 	static LogbookLogLink : string = "https://log.concept2.com/profile/1054524/log";
-	static Logbook2kLink : string = "https://log.concept2.com/rankings/2025/rower/2000?age=&weight=&gender=&rower=rower&status=verified&page=10#:~:text=Dennis%20Pohl";
-	static Logbook5kLink : string = "https://log.concept2.com/rankings/2025/rower/5000?age=&weight=&gender=&rower=rower&status=verified&page=5#:~:text=Dennis%20Pohl";
-	static Logbook10kLink : string = "https://log.concept2.com/rankings/2025/rower/10000?age=&weight=&gender=&rower=rower&status=verified&page=4#:~:text=Dennis%20Pohl";
+	static Logbook2kLink : string = "https://log.concept2.com/rankings/2025/rower/2000?age=&weight=&gender=&rower=rower&status=verified&page=10#492&:~:text=Dennis%20Pohl";
+	static Logbook5kLink : string = "https://log.concept2.com/rankings/2025/rower/5000?age=&weight=&gender=&rower=rower&status=verified&page=5#233&:~:text=Dennis%20Pohl";
+	static Logbook10kLink : string = "https://log.concept2.com/rankings/2025/rower/10000?age=&weight=&gender=&rower=rower&status=verified&page=4#194:~:text=Dennis%20Pohl";
 }
 
 export default Rowing;

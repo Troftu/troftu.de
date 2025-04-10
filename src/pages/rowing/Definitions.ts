0108fd5f0cdd1b4922b7963b6d1826131fdb1aed
Dennis Pohl <Troftu@users.noreply.github.com>
2025-04-10 19:14:49 +0200

    Update 10k link

--- src/pages/rowing/Definitions.ts
+++ src/pages/rowing/Definitions.ts
@@ -3,7 +3,7 @@ class Rowing {
 	static LogbookLogLink : string = "https://log.concept2.com/profile/1054524/log";
 	static Logbook2kLink : string = "https://log.concept2.com/rankings/2025/rower/2000?age=&weight=&gender=&rower=rower&status=verified&page=15#709&:~:text=Dennis%20Pohl";
 	static Logbook5kLink : string = "https://log.concept2.com/rankings/2025/rower/5000?age=&weight=&gender=&rower=rower&status=verified&page=5#247&:~:text=Dennis%20Pohl";
-	static Logbook10kLink : string = "https://log.concept2.com/rankings/2025/rower/10000?age=&weight=&gender=&rower=rower&status=verified&page=5#218&:~:text=Dennis%20Pohl";
+	static Logbook10kLink : string = "https://log.concept2.com/rankings/2025/rower/10000?age=&weight=&gender=&rower=rower&status=verified&page=4#191&:~:text=Dennis%20Pohl";
 	static Logbook1hLink : string = "https://log.concept2.com/rankings/2025/rower/60?age=&weight=&gender=&rower=rower&status=verified&page=3#121&:~:text=Dennis%20Pohl";
 }
 
